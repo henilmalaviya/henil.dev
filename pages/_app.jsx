36504cbdc02131fe0aaba36d935779e340e403ca
henil0604 <henilmalaviya06@gmail.com>
2022-08-01 10:13:27 +0530

    implemented react query

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,30 +1,35 @@
-import { AppProps } from "next/app";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App(props) {
   const { Component, pageProps } = props;
 
   return (
     <>
-      <Head>
-        <title>Page title</title>
-        <meta
-          name='viewport'
-          content='minimum-scale=1, initial-scale=1, width=device-width'
-        />
-      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Head>
+          <title>Page title</title>
+          <meta
+            name='viewport'
+            content='minimum-scale=1, initial-scale=1, width=device-width'
+          />
+        </Head>
 
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: "light",
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </QueryClientProvider>
     </>
   );
 }
