27f0d33dee1be394c645f45a47110403b116c367
henil0604 <henilmalaviya06@gmail.com>
2022-08-01 09:33:32 +0530

    add path alias

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import xw from 'xwind'
-import ButtonReact from '../components/ButtonReact'
-import ButtonStyled from '../components/ButtonStyled'
+import ButtonReact from '@/components/ButtonReact'
+import ButtonStyled from '@/components/ButtonStyled'
 
 const Index = () => (
   <div css={xw`grid justify-center items-center h-screen space-y-20`}>
