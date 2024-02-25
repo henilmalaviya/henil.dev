2dece82a92e54a5f124dd7fd8e99cd9691ab8ac4
Henil Malaviya <henilmalaviya06@gmail.com>
2024-02-25 17:24:45 +0530

    recreate template

--- src/lib/components/ui/Icon.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import Icon from "@iconify/svelte";
-
-    export let icon: string;
-</script>
-
-<div class="w-fit h-fit cursor-pointer">
-    <Icon {icon} />
-</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,1 @@
-<script>
-    import { browser } from "$app/environment";
-    import { darkMode } from "$lib/store";
-    import setDarkMode from "$lib/utils";
-    import "../app.postcss";
-
-    if (browser) {
-        setDarkMode($darkMode);
-    }
-</script>
-
-<slot />
+<script>import "../app.pcss";</script><slot></slot>
