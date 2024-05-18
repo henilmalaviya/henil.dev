4a109b7cee0969d108f3f024554a31d682bb67fd
Henil Malaviya <hmalaviya@ssessa.com>
2024-05-18 11:33:31 +0530

    remove activity files

--- src/routes/RightColumn.svelte
+++ src/routes/RightColumn.svelte
@@ -1,30 +1,28 @@
 <script>
-    import AboutRC from "./About.RC.svelte";
-    import ActivityRC from "./Activity.RC.svelte";
+  import AboutRC from "./About.RC.svelte";
 </script>
 
 <div class="pt-0 pb-16 lg:pt-32 lg:w-1/2 flex flex-col">
-    <AboutRC />
-    <div class="my-3"></div>
-    <!-- <ActivityRC /> -->
-    <div class="my-16"></div>
-    <hr class="mb-4" />
-    <div class="text-sm">
-        <div>
-            Built with
-            <a target="_blank" href="https://kit.svelte.dev">SvelteKit</a>
-            +
-            <a href="https://tailwindcss.com" target="_blank">Tailwind CSS</a>
-        </div>
-        <div>
-            Design <b>heavily</b> Inspired by
-            <a href="https://brittanychiang.com" target="_blank">
-                brittanychiang.com
-            </a>
-        </div>
-        <div>
-            Deployed with
-            <a target="_blank" href="https://vercel.com">Vercel</a>
-        </div>
+  <AboutRC />
+  <div class="my-3"></div>
+  <div class="my-16"></div>
+  <hr class="mb-4" />
+  <div class="text-sm">
+    <div>
+      Built with
+      <a target="_blank" href="https://kit.svelte.dev">SvelteKit</a>
+      +
+      <a href="https://tailwindcss.com" target="_blank">Tailwind CSS</a>
     </div>
+    <div>
+      Design <b>heavily</b> Inspired by
+      <a href="https://brittanychiang.com" target="_blank">
+        brittanychiang.com
+      </a>
+    </div>
+    <div>
+      Deployed with
+      <a target="_blank" href="https://vercel.com">Vercel</a>
+    </div>
+  </div>
 </div>
--- src/routes/api/github/type.ts
+++
@@ -1,9 +0,0 @@
-import type { getLastNCommits, getMostActiveRepositories } from "./util";
-
-export type RecentCommit = Awaited<ReturnType<typeof getLastNCommits>>[number];
-export type ActiveRepo = Awaited<ReturnType<typeof getMostActiveRepositories>>[number];
-
-export interface GithubResponse {
-    recent_commits: RecentCommit[],
-    active_repos: ActiveRepo[]
-}
