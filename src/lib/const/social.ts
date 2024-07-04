714d981b4a9aed6292142f7a5c0ee14210393481
Henil Malaviya <henilm.dev@gmail.com>
2024-07-04 13:37:41 +0530

    chore: update email

--- src/lib/const/social.ts
+++ src/lib/const/social.ts
@@ -1,4 +1,4 @@
-export const Email = 'me@henil.xyz';
+export const Email = 'hello@henil.dev';
 export const GithubUsername = 'henil0604';
 export const XUsername = 'henilmalaviya';
 export const LinkedInUsername = 'henil-malaviya';
