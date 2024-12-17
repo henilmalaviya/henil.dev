d85867054303360a4216db4a8df4063bab8fa14b
Henil Malaviya <henilm.dev@gmail.com>
2024-12-17 19:11:36 +0530

    feat: change about section

--- src/routes/About.RC.svelte
+++ src/routes/About.RC.svelte
@@ -4,31 +4,25 @@
 	<div class="flex flex-col text-base gap-4">
 		<div>
 			<span>
-				In 2020, I jumped into web development—and I've been loving it
-				ever since. I started with the usual suspects: JavaScript, PHP,
-				and Node.js, building random projects just to see what I could
-				make. Along the way, I discovered TypeScript, because, let's be
-				honest, writing safer code just makes life easier. Then came
-				frameworks like React, Vue, and meta-frameworks like Next.js and
-				Nuxt.js—which felt like leveling up in a video game.
-			</span>
-		</div>
-		<div>
-			<span>
-				As I dug deeper, I became obsessed with web security and learned
-				about server-client communication and authentication. That's
-				when I realized I actually love backend way more than frontend.
-				So, I dove into tools like Express.js, Hono, and Elysia, and
-				that's pretty much where I've been since.
-			</span>
-		</div>
-		<div>
-			<span>
-				Now, Svelte has stolen my developer heart. It's so simple and
-				efficient—it's like the universe gave me a framework that just
-				makes sense. These days, I use Svelte for everything, and I'm
-				not looking back.
-			</span>
+				I mess around with <b title="eww javascript">TypeScript</b>,
+				<b title="Bro is on vacation or something?">SvelteKit</b>, and
+				<b title="Eli-what?">Elysia</b>. I love building stuff that's
+				actually useful.
+				<span title="not bitcoin bruh">Cryptography</span>
+				fascinates me.
+				<span title="a kid">I break things just to fix them better</span
+				>, and I'm always playing with new tech. Oh, and I like doing
+				everything from scratch...
+				<span title="contradictory"
+					>but yeah, I use frameworks to build stuff too
+				</span>.
+				<span title="better"
+					>I like React, but I don't use it much these days.</span
+				>
+				I'm also
+				<i>way too into</i>
+				<span title="I call it lunix ;)">Linux</span> and open-source softwares.</span
+			>
 		</div>
 	</div>
 </div>
