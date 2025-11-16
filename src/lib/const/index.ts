de91fd77665185eae0e1903d7832cbafc2507d0f
Henil Malaviya <henilm.dev@gmail.com>
2025-11-17 00:11:58 +0530

    redesign

--- src/lib/const/index.ts
+++ src/lib/const/index.ts
@@ -5,3 +5,44 @@ export const AGE = new Date().getFullYear() - BIRTH_YEAR;
 
 export const GITHUB_URL = 'https://github.com/henilmalaviya';
 export const X_URL = 'https://x.com/henilmalaviya';
+
+export const EXPERIENCE = [
+	{
+		company: 'ssessa',
+		role: 'Developer Intern',
+		period: 'Oct 2025 - Present',
+		description:
+			'Developing full-stack project using Next.js and Typescript.',
+		href: 'https://ssessa.com',
+	},
+	{
+		company: 'ssessa',
+		role: 'React Intern',
+		period: 'May 2024 - Oct 2024',
+		description: 'Developed and maintained React-based project.',
+		href: 'https://ssessa.com',
+	},
+];
+
+export const PROJECTS = [
+	{
+		name: 'Link API',
+		description:
+			'A simple anonymous URL shortening API built with speed in mind.',
+		href: 'https://api.link.henil.dev/swagger',
+		github: 'https://github.com/henilmalaviya/link-api',
+	},
+	{
+		name: 'GOL',
+		description:
+			"A small, concurrent-safe core library for Conway's Game of Life in Go.",
+		github: 'https://github.com/henilmalaviya/gol',
+	},
+	{
+		name: "Conway's Balcony",
+		description:
+			'A public web interface to watch and interact with Conway’s Game of Life simulation in real time.',
+		href: 'https://balcony.henil.dev',
+		github: 'https://github.com/henilmalaviya/conway-balcony',
+	},
+];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,174 @@
-<script>
+<script lang="ts">
+	import { EMAIL, EXPERIENCE, GITHUB_URL, PROJECTS, X_URL } from '$lib/const';
+	import Icon from '@iconify/svelte';
 </script>
 
-<div>Hello</div>
+{#snippet Header()}
+	<div class="flex items-end justify-between">
+		<a href="/">
+			<h1 class="text-lg font-medium">Henil Malaviya</h1>
+		</a>
+		<div class="flex gap-1 text-sm">
+			<a href={GITHUB_URL} class="muted" target="_blank">Github</a>
+			<a href={X_URL} class="muted" target="_blank">X</a>
+			<a href="mailto:{EMAIL}" class="muted" target="_blank">Email</a>
+		</div>
+	</div>
+{/snippet}
+
+{#snippet Card({
+	title,
+	href,
+	description,
+	subTitle,
+	links,
+}: {
+	title: string;
+	href?: string;
+	subTitle?: string;
+	description?: string;
+	links?: ({ icon: string; href: string } | null)[];
+})}
+	<div class="flex w-full flex-col gap-0 text-left">
+		<div class="flex w-full justify-between">
+			<!-- title -->
+			<h3>{title}</h3>
+			{#if links || href}
+				<div class="flex gap-1">
+					{#if href}
+						<a {href} target="_blank" class="muted">
+							<Icon icon="mdi:link-variant" width={18} />
+						</a>
+					{/if}
+					{#each links as link}
+						{#if !link}{:else}
+							<a href={link.href} target="_blank" class="muted"
+								><Icon icon={link.icon} width={18} /></a
+							>
+						{/if}
+					{/each}
+				</div>
+			{/if}
+		</div>
+		{#if subTitle}
+			<p class="text-muted text-xs">{subTitle}</p>
+		{/if}
+		<p class="text-muted mt-2 text-sm">
+			{description}
+		</p>
+	</div>
+{/snippet}
+
+<div class="mx-auto flex w-full max-w-2xl flex-col gap-8 px-4 py-16">
+	{@render Header()}
+
+	<div class="mb-4 leading-7">
+		<p>
+			I'm full-stack developer and junior undergrad student, currently
+			interning at <a
+				class="accent"
+				href="https://ssessa.com"
+				target="_blank">ssessa</a
+			>. I like to design and build privacy-focused scalable apps for
+			modern web. My core stack is Typescript, ElysiaJS, Go and Svelte.
+		</p>
+
+		<br />
+
+		<p>
+			My programming journey started with small PHP projects and a lot of
+			phpMyAdmin during the covid days. I moved into NodeJS and React, and
+			around then, TypeScript became my tool of choice. Playing with P5.js
+			got me hooked on making simulations, and I kept exploring from
+			there. After that, I picked up ExpressJS, HonoJS, and now, on
+			ElysiaJS for backend work.
+		</p>
+
+		<br />
+
+		<p>
+			I've also messed around a bit with Vue.js, Nuxt.js, Rust, Docker,
+			and networking. Recently, I've been diving into cryptography and
+			digging deeper into Go for backend systems and low-level tools.
+		</p>
+
+		<br />
+
+		<p>
+			Apart from coding, you would find me playing factorio, listening to
+			music, exploring topics in evolutionary biology and <span
+				title="OperatorDrewski XD"
+			>
+				watching military simulation gameplay
+			</span>.
+		</p>
+	</div>
+
+	<div class="grid gap-8 sm:grid-cols-2">
+		<div class="flex flex-col gap-4">
+			<h2 class="text-muted font-medium">Experience</h2>
+			<ul
+				class="group/list flex flex-col [&>li]:py-2 [&>li]:transition-opacity hover:[&>li]:opacity-50 hover:[&>li:hover]:opacity-100"
+			>
+				{#each EXPERIENCE as experience}
+					<li>
+						{@render Card({
+							title: `${experience.role} @ ${experience.company}`,
+							href: experience.href,
+							subTitle: experience.period,
+							description: experience.description,
+						})}
+					</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="flex flex-col gap-4">
+			<h2 class="text-muted font-medium">Projects</h2>
+			<ul
+				class="group/list flex flex-col [&>li]:py-2 [&>li]:transition-opacity hover:[&>li]:opacity-50 hover:[&>li:hover]:opacity-100"
+			>
+				{#each PROJECTS as project}
+					<li>
+						{@render Card({
+							title: project.name,
+							href: project.href,
+							description: project.description,
+							links: [
+								{ href: project.github, icon: 'mdi:github' },
+							],
+						})}
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+
+	<div class="text-muted mt-32 flex items-end justify-between text-xs">
+		<span>&copy; 2025 henilmalaviya</span>
+		<div class="flex flex-col">
+			<span
+				>Inspired by <a
+					href="https://heywinit.me"
+					target="_blank"
+					class="muted"
+					rel="noopener noreferrer">heywinit.me</a
+				></span
+			>
+			<span
+				>Powered by <a
+					href="https://svelte.dev"
+					target="_blank"
+					class="muted"
+					rel="noopener noreferrer">Svelte</a
+				>
+				and
+				<a
+					href="https://vercel.com"
+					target="_blank"
+					class="muted"
+					rel="noopener noreferrer">Vercel</a
+				></span
+			>
+		</div>
+	</div>
+</div>
