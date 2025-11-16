337b596a6cf2bb010d0cc455ea757fdd9d2c09ab
Henil Malaviya <henilm.dev@gmail.com>
2025-11-16 09:18:49 +0530

    reset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,7 @@
 <script lang="ts">
-	import GameOfLife from '$lib/components/GameOfLife.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link
-		rel="preconnect"
-		href="https://fonts.gstatic.com"
-		crossorigin="anonymous"
-	/>
-	<link
-		href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&display=swap"
-		rel="stylesheet"
-	/>
-</svelte:head>
-
-<GameOfLife />
 
 {@render children()}
